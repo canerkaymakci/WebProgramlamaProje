ac28f31fa70bf975f7bae59f6ba0c2ddc11e7b40
canerkaymakci <canerkaymakci@gmail.com>
2023-12-21 22:29:08 +0300

    BaseRepository Created

---
+++ WebProgramlamaProje/WebProgramlamaProje/Repository/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebProgramlamaProje.Models;
+
+namespace WebProgramlamaProje.Repository
+{
+	public abstract class BaseRepository<TEntity,TContext>: IBaseRepository<TEntity> where TEntity:class, IBaseEntity where TContext:DbContext
+	{
+        private readonly TContext _context;
+
+		public BaseRepository(TContext context)
+		{
+            _context = context;
+		}
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> DeleteAsync(Guid Id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(Id);
+            if (entity == null)
+                return entity;
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(TEntity entity)
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid Id)
+        {
+            return await _context.Set<TEntity>().FindAsync(Id);
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
+
---
+++ WebProgramlamaProje/WebProgramlamaProje/Repository/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WebProgramlamaProje.Models;
+
+namespace WebProgramlamaProje.Repository
+{
+	public interface IBaseRepository<TEntity> where TEntity:class,IBaseEntity
+	{
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(Guid Id);
+        Task<List<TEntity>> GetAllAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(Guid Id);
+
+    }
+}
+
